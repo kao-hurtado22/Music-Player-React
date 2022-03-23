db089374a3804e4fdf1934e142a653c5b882c199
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-03-23 16:03:45 +0000

    inicio music player

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
 const Home = () => {
+	const [songs, setSongs] = useState([
+		{
+			title: "",
+			artist: "Artist 1",
+			img_src: "",
+			src: "",
+		},
+		{
+			title: "",
+			artist: "Artist 2",
+			img_src: "",
+			src: "",
+		},
+		{
+			title: "",
+			artist: "Artist 3",
+			img_src: "",
+			src: "",
+		},
+		{
+			title: "",
+			artist: "Artist 4",
+			img_src: "",
+			src: "",
+		},
+		{
+			title: "",
+			artist: "Artist 5",
+			img_src: "",
+			src: "",
+		},
+		{
+			title: "",
+			artist: "Artist 6",
+			img_src: "",
+			src: "",
+		},
+	])
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
 		</div>
 	);
 };
